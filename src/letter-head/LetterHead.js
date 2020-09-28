ff63ca5ac49e62c86363ba214411d3643c8e9c1e
ckjdelhi <chandan.krk@gmail.com>
2020-09-28 13:52:02 +0530

    changes

--- src/letter-head/LetterHead.js
+++ src/letter-head/LetterHead.js
@@ -7,14 +7,8 @@ class LetterHead extends React.Component {
     render(){
         return (
             <div class="letterHeadContainer">
-                <p>Hello {this.props.location.title},</p>
+                <p class="highlighter">{this.props.location.title},</p>
                 <p>{this.props.location.description}</p>
-                <ol class="highlighter">
-                    <li>Either this isn't a priority for you and your company at the moment</li>
-                    <li>You've been busy and we should keep trying to connect</li>
-                </ol>
-                <p>If the answer is option one, I won't take up any more of your time. If the answer is option two, do you have time to connect tomorrow?</p>
-                <p>Thanks,<br/>[Your name]</p>
             </div>
         )
     }
